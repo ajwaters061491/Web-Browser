9262a8c2d74f56806f9cfbbdcd59c9324424b886
Alvin Waters <alvin.waters0614@gmail.com>
2019-05-08 03:09:27 -0400

    Add project files.

---
+++ Web Broser/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Web_Broser
+{
+    partial class frmWebBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtAddressBar = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(307, 9);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(47, 23);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(12, 9);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Location = new System.Drawing.Point(93, 9);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 23);
+            this.btnForward.TabIndex = 2;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(226, 9);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(1057, 9);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(42, 23);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtAddressBar
+            // 
+            this.txtAddressBar.Location = new System.Drawing.Point(360, 9);
+            this.txtAddressBar.Name = "txtAddressBar";
+            this.txtAddressBar.Size = new System.Drawing.Size(691, 20);
+            this.txtAddressBar.TabIndex = 6;
+            this.txtAddressBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddressBar_KeyDown);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 38);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1087, 585);
+            this.webBrowser.TabIndex = 7;
+            // 
+            // frmWebBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 635);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.txtAddressBar);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnHome);
+            this.Name = "frmWebBrowser";
+            this.Text = "Web Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtAddressBar;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ Web Broser/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Web_Broser
+{
+    public partial class frmWebBrowser : Form
+    {
+        public frmWebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoHome(); //home button
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.IsBusy)
+            {
+                webBrowser.Stop(); //stop loading if something is loading
+            }
+            else
+            {
+                webBrowser.Refresh(); //refresh page if nothing is loading
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+            {
+                webBrowser.GoBack(); //goes back if possible
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+            {
+                webBrowser.GoForward(); //goes forward if possible
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string webpage = txtAddressBar.Text.Trim();
+            webBrowser.Navigate(webpage); //navigates to the web page listed in the web address bar 
+        }
+
+        private void txtAddressBar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string webpage = txtAddressBar.Text.Trim();
+                webBrowser.Navigate(webpage); //navigates to the web page listed in the web address bar (pressing enter)
+            }
+        }
+    }
+}
